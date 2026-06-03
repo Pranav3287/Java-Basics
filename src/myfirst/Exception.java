82daec9491857d832e17276c47150e6be2754980
Pranav3287 <pranavyedage3287@gmail.com>
2026-06-03 14:39:43 +0530

    Good

--- src/myfirst/Exception.java
+++ src/myfirst/Exception.java
@@ -3,8 +3,25 @@ package myfirst;
 public class Exception {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+   try {
+		int a = 1;
+		int b = 0;
+		int c = a / b;
+		System.out.println(c);
 
+	}catch(ArithmeticException e) {
+		System.out.println("ArithmeticException");
+		
+	}
+   catch(ClassCastException e) {
+		System.out.println("ClassCastException");
+		
+	}
+   finally {
+	   
+	   System.out.println(" from finally");
+   }
 	}
 
 }
+
