1b3b1b8745cc8901422632dc66114cf5270616cf
Pranav3287 <pranavyedage3287@gmail.com>
2026-06-06 16:03:04 +0530

    Done

--- src/Oopsconcepts/Object1.java
+++ src/Oopsconcepts/Object1.java
@@ -7,12 +7,10 @@ class Tatacar{
 	}
 }
 public class Object1 {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Tatacar t1=new Tatacar();
 		t1.play();
 
 	}
-
 }
