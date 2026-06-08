738cad90f3d96b6c9e583d236125d7775efd1962
Pranav3287 <pranavyedage3287@gmail.com>
2026-06-08 19:55:38 +0530

    Patterns

---
+++ src/Pattern/PatternSolid.java
@@ -0,0 +1,33 @@
+package Pattern;
+
+public class PatternSolid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=4;
+		
+		/*
+		 *     ****
+		 *    ****
+		 *   ****
+		 *  ****
+		 * ****
+		 */
+		
+		for(int i=1;i<=n;i++) {
+			
+			//spaces
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			
+			//stars
+			for(int j=1;j<=5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
